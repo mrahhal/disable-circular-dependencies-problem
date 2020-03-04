66dd6cae0e807ddcc2bece1feea1c10546eacc88
Mohammad Rahhal <m.r992@hotmail.com>
2020-03-04 08:53:35 +0200

    Add @angular-builders/custom-webpack

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { some } from './some';
+
+export const here = 2;
+
 @NgModule({
   declarations: [
     AppComponent
@@ -14,3 +18,9 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export class AppClass {
+  getSome() {
+    return some;
+  }
+}
---
+++ src/app/some.ts
@@ -0,0 +1,9 @@
+import { here } from './app.module';
+
+export const some = 3;
+
+export class SomeClass {
+  getHere() {
+    return here;
+  }
+}
